68fecb276f1942cf99295ca55d1dc9badb5b3fec
Josip Josic <71074064+Josipjosic@users.noreply.github.com>
2022-02-10 14:57:46 +0100

    added few elements, button stylings

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <Container className="container">
+      <Container style={{ width: 940, padding: 0 }} className="container-main">
+        <div className="header-main">
+          <div className=" header-small"></div>
+          <div className=" header">
+            <h1>Skup drugih elemenata na stranici</h1>
+          </div>
+        </div>
         <div className="row">
           <div className="col-8">
             <Slider />
@@ -21,7 +27,16 @@ function App() {
             </p>
           </div>
         </div>
+        <div className="d-flex flex-row-reverse row-button">
+          <div className="col-5">
+            <button type="button" className=" btn-1"></button>
+            <button type="button" className="btn-2"></button>
+          </div>
+        </div>
       </Container>
+      <div className="footer">
+        <h1>Skup drugih elemenata na stranici</h1>
+      </div>
     </div>
   );
 }
--- src/components/slider.js
+++ src/components/slider.js
@@ -1,32 +1,140 @@
 import React from "react";
-import './slider.css'
+import "./slider.css";
 import { Container } from "react-bootstrap";
 
 function Slider() {
   return (
-    <Container>
-        <div className="col-8">
-        <img src={require("../images/slider-image-1.jpg")} alt="pic1" style={{height: 200, width:260}}></img>
-          <img src={require("../images/slider-image-2.jpg")} alt="pic2" style={{height: 200, width:120}}></img>
-          <img src={require("../images/slider-image-3.jpg")} alt="pic3" style={{height: 200, width:180}}></img>
-          <img src={require("../images/slider-image-4.jpg")} alt="pic4" style={{height: 200, width:220}}></img>
-          <img src={require("../images/slider-image-5.jpg")} alt="pic5" style={{height: 200, width:260}}></img>
-          <img src={require("../images/slider-image-6.jpg")} alt="pic6" style={{height: 200, width:120}}></img>
-          <img src={require("../images/slider-image-7.jpg")} alt="pic7" style={{height: 200, width:180}}></img>
-          <img src={require("../images/slider-image-8.jpg")} alt="pic8" style={{height: 200, width:180}}></img>
-          <img src={require("../images/slider-image-9.jpg")} alt="pic9" style={{height: 200, width:220}}></img>
-        </div>
-        <div  className="col-6">
-        <img src={require("../images/slider-image-1.jpg")} alt="pic1" style={{height: 200, width:260}}></img>
-          <img src={require("../images/slider-image-2.jpg")} alt="pic2" style={{height: 200, width:120}}></img>
-          <img src={require("../images/slider-image-3.jpg")} alt="pic3" style={{height: 200, width:180}}></img>
-          <img src={require("../images/slider-image-4.jpg")} alt="pic4" style={{height: 200, width:220}}></img>
-          <img src={require("../images/slider-image-5.jpg")} alt="pic5" style={{height: 200, width:260}}></img>
-          <img src={require("../images/slider-image-6.jpg")} alt="pic6" style={{height: 200, width:120}}></img>
-          <img src={require("../images/slider-image-7.jpg")} alt="pic7" style={{height: 200, width:180}}></img>
-          <img src={require("../images/slider-image-8.jpg")} alt="pic8" style={{height: 200, width:180}}></img>
-          <img src={require("../images/slider-image-9.jpg")} alt="pic9" style={{height: 200, width:220}}></img>
-        </div>
+    <Container className="container">
+      <ul className="col-12 d-flex flex-row-reverse">
+        <li>
+          <img
+            src={require("../images/slider-image-5.jpg")}
+            alt="pic5"
+            style={{ height: 200, width: 260 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-8.jpg")}
+            alt="pic8"
+            style={{ height: 200, width: 180 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-9.jpg")}
+            alt="pic9"
+            style={{ height: 200, width: 220 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-6.jpg")}
+            alt="pic4"
+            style={{ height: 200, width: 120 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-7.jpg")}
+            alt="pic5"
+            style={{ height: 200, width: 180 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-2.jpg")}
+            alt="pic6"
+            style={{ height: 200, width: 120 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-5.jpg")}
+            alt="pic7"
+            style={{ height: 200, width: 180 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-4.jpg")}
+            alt="pic8"
+            style={{ height: 200, width: 180 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-1.jpg")}
+            alt="pic9"
+            style={{ height: 200, width: 220 }}
+          ></img>
+        </li>
+      </ul>
+      <ul className="col-9 d-flex flex-row-reverse">
+        <li>
+          <img
+            src={require("../images/slider-image-1.jpg")}
+            alt="pic1"
+            style={{ height: 200, width: 260 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-2.jpg")}
+            alt="pic2"
+            style={{ height: 200, width: 120 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-3.jpg")}
+            alt="pic3"
+            style={{ height: 200, width: 180 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-4.jpg")}
+            alt="pic4"
+            style={{ height: 200, width: 220 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-5.jpg")}
+            alt="pic5"
+            style={{ height: 200, width: 260 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-6.jpg")}
+            alt="pic6"
+            style={{ height: 200, width: 120 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-7.jpg")}
+            alt="pic7"
+            style={{ height: 200, width: 180 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-8.jpg")}
+            alt="pic8"
+            style={{ height: 200, width: 180 }}
+          ></img>
+        </li>
+        <li>
+          <img
+            src={require("../images/slider-image-9.jpg")}
+            alt="pic9"
+            style={{ height: 200, width: 220 }}
+          ></img>
+        </li>
+      </ul>
     </Container>
   );
 }
